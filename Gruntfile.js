b5bdd689f36964deb86674a8f13753e465228413
ju.uyeong <ju.uyeong@nhn.com>
2015-01-20 10:50:52 +0900

    프로젝트 준비, 기반 구조 생성 #1

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+        },
+        karma: {
+            test: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        jsdoc : {
+            dist: {
+                src: ['README.md'],
+                options: {
+                    template: "node_modules/jaguarjs-jsdoc",
+                    encoding: "utf8",
+                    destination: "docs",
+                    recurse: true,
+                    private: true,
+                    configure: 'jsdoc.conf.json'
+                }
+            }
+        },
+        concat : {
+            dist : {
+                src : [
+                    'src/**/*.js',
+                    'src/<%= pkg.name %>.js'
+                ],
+                dest: 'build/<%= pkg.name %>-<%= pkg.version %>.js'
+            }
+        },
+        uglify : {
+            dist : {
+                files : {
+                    'build/<%= pkg.name %>-<%= pkg.version %>.min.js' : [
+                        'build/<%= pkg.name %>-<%= pkg.version %>.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Test task(s).
+    grunt.registerTask('test', ['jshint', 'karma']);
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'karma', 'jsdoc', 'concat', 'uglify']);
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# jindo.router
+Javascript Routers for Jindo Library.
+
+## Getting Started
+_(Coming soon)_
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2015 NHN Technology Services Corp.
+Licensed under the NTS license LGPL v2.
